affc7207f07417af74a402544f24b414ab1dfbf4
Marcin Chalecki <alcaster0@gmail.com>
2018-06-02 17:16:09 +0200

    Improved logging methods.

--- csv_extractor.go
+++ csv_extractor.go
@@ -6,37 +6,39 @@ import (
 	"fmt"
 	"os/exec"
 	"bytes"
-	"log"
 	"sync"
+	log "github.com/sirupsen/logrus"
 )
 
 func extractCsvs(host string, columnsArrays [] string, done chan<- string, startDate time.Time, endDate time.Time, tablename string, wg sync.WaitGroup) () {
 	currentDate := startDate
 	for currentDate.Before(endDate) {
 		dateStr := currentDate.Format("2006-01-02")
-		filename := getSingleCsvOnServer(host, dateStr, columnsArrays, tablename)
-		done <- filename
+		err, filename := getSingleCsvOnServer(host, dateStr, columnsArrays, tablename)
+		if err == nil {
+			done <- filename
+		}
 		currentDate = currentDate.AddDate(0, 0, 1)
 	}
 	done <- "done"
 	wg.Done()
 }
 
-func getSingleCsvOnServer(host string, dateStr string, columns []string, tablename string) (string) {
+func getSingleCsvOnServer(host string, dateStr string, columns []string, tablename string) (error, string) {
 	columnsStr := strings.Join(columns, ",")
 	filename := fmt.Sprintf("/tmp/%s___%s.csv", tablename, dateStr)
 	command := fmt.Sprintf(`hive -e 'SELECT %s `+
 		`FROM %s where day="%s";'`+
 		`| sed 's/[\t]/,/g' > %s`, columnsStr, tablename, dateStr, filename)
-	fmt.Println("Extracting csv: " + filename)
-	fmt.Println("Running command ", command)
+	log.Info("Extracting csv: " + filename)
+	log.Debug("Command: " + command)
 	cmd := exec.Command("ssh", host, command)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf("Didn't extracted %s, error:%v", filename, err)
 	}
-	fmt.Println("Succesfully extracted csv: " + filename)
-	return filename
+	log.Info("Successfully extracted csv: " + filename)
+	return err, filename
 }
--- csv_server_transport.go
+++ csv_server_transport.go
@@ -4,43 +4,47 @@ import (
 	"os/exec"
 	"fmt"
 	"bytes"
-	"log"
 	"sync"
+	log "github.com/sirupsen/logrus"
 )
 
 func downloaderCsv(host string, done <-chan string, to string, wg sync.WaitGroup) () {
 	for {
 		csvFilename := <-done
+		log.Debug("Received:" + csvFilename)
 		if csvFilename == "done" {
 			break
 		}
 		downloadSingleCsv(host, csvFilename, to)
 		deleteRemoteCsv(host, csvFilename)
 	}
+    log.Info("Downloader finished")
 	wg.Done()
 }
 func downloadSingleCsv(host string, csvFilename string, to string) () {
 	file := fmt.Sprintf("%s:%s", host, csvFilename)
-	fmt.Printf("Downloading %s, to %s", file, to)
+	log.Infof("Downloading %s, to %s", file, to)
 	cmd := exec.Command("scp", file, to)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf("Didn't downloaded %s, error: %v", file, err)
+		return
 	}
 	fmt.Println("Succesfully downloaded csv: " + csvFilename)
 
 }
 func deleteRemoteCsv(host string, csvFilename string) () {
-	fmt.Println("Deleting ", csvFilename)
+	log.Info("Deleting ", csvFilename)
 	command := fmt.Sprintf("rm %s", csvFilename)
 	cmd := exec.Command("ssh", host, command)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf("Didn't removed %s, error: %v", csvFilename, err)
+		return
 	}
-	fmt.Printf("Succesfully removed csv %s from the server\n", csvFilename)
+	log.Infof("Successfully removed csv %s from the server\n", csvFilename)
 }
